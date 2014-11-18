d3d9148701b3a08f8002cb1d36b1f38de1d22b97
Patrick <patricke@iastate.edu>
2014-11-18 11:34:01 -0600

    Top level class of controller for receiving search type from jsp and returning results as JSON.

---
+++ EM/src/edu/iastate/cs/proj461/SearchResults.java
@@ -0,0 +1,15 @@
+package edu.iastate.cs.proj461;
+
+public class SearchResults {
+	
+	private String searchType;
+
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+}
