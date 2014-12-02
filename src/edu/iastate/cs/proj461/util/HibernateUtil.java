6fefa6f2e61b0df59820edb7b93f29f0b5b82b43
Patrick <patricke@iastate.edu>
2014-12-02 00:47:16 -0600

    HibernateUtil class to create sessionFactory for database access.

---
+++ src/edu/iastate/cs/proj461/util/HibernateUtil.java
@@ -0,0 +1,28 @@
+package edu.iastate.cs.proj461.util;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+	private static SessionFactory serviceRegistry;
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
+			sessionFactory = configuration.buildSessionFactory(ssrb.build());
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+		}
+	}
+	
+	public static synchronized SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+}
