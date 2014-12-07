76cd141d428e6445a4a88bd382717561b0348e02
Patrick <patricke@iastate.edu>
2014-12-06 22:20:49 -0600

    Package for testing utilities

---
+++ src/edu/iastate/cs/proj461/test/HibernateTest.java
@@ -0,0 +1,26 @@
+package edu.iastate.cs.proj461.test;
+
+import java.util.Date;
+import java.util.List;
+
+import edu.iastate.cs.proj461.util.HibernateUtil;
+import edu.iastate.cs.proj461.video.Video;
+import edu.iastate.cs.proj461.video.VideoDAO;
+import edu.iastate.cs.proj461.video.VideoDAOImpl;
+
+public class HibernateTest {
+		
+	public static void main(String[] args) {
+		VideoDAO videoDAO = new VideoDAOImpl(HibernateUtil.getSessionFactory());
+		
+		Date date = new Date();
+		
+		List<Video> resultList = (List<Video>) videoDAO.findVideoByCapturedDateTime(date.toString(), 
+				true);
+	}
+
+	//Fetch data from database here
+	//	Cast to corresponding class
+
+
+}
