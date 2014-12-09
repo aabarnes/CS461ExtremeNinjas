2480ea4478c4a311ae7a3839d7af9eb8bc085148
Patrick <patricke@iastate.edu>
2014-12-09 12:39:38 -0600

    Added checking of user/password existence within database.

--- src/edu/iastate/cs/proj461/LoginAction.java
+++ src/edu/iastate/cs/proj461/LoginAction.java
@@ -1,22 +1,51 @@
 package edu.iastate.cs.proj461;
 
+import java.util.Map;
+
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+
+import edu.iastate.cs.proj461.user.User;
+import edu.iastate.cs.proj461.user.UserDAO;
+import edu.iastate.cs.proj461.user.UserDAOImpl;
+import edu.iastate.cs.proj461.util.HibernateUtil;
+import edu.iastate.cs.proj461.video.VideoDAO;
+import edu.iastate.cs.proj461.video.VideoDAOImpl;
  
-public class LoginAction extends ActionSupport{
+public class LoginAction extends ActionSupport implements SessionAware{
     private String username;
     private String password;
+    
+    private SessionMap<String, Object> sessionMap;
+    
+    //private User user = new User();
  
     public String execute() {
  
-        if (this.username.equals("admin") 
-                && this.password.equals("admin123")) {
-            return "success";
-        } else {
-            addActionError(getText("error.login"));
-            return "error";
-        }
+		UserDAO userDAO = new UserDAOImpl(HibernateUtil.getSessionFactory());
+		User userDB = userDAO.getUserByCredentials(username, password);
+		if(userDB == null)
+			return Action.ERROR;
+		else {
+			sessionMap.put("username", username);
+			return Action.SUCCESS;
+		}
     }
+
+	@Override
+	public void setSession(Map<String, Object> map) {
+    	sessionMap = (SessionMap<String, Object>) map;
+	}
+	
+	public String logout() {
+		sessionMap.invalidate();
+		return Action.SUCCESS;
+	}
  
+    /*
     public String getUsername() {
         return username;
     }
@@ -32,4 +61,5 @@ public class LoginAction extends ActionSupport{
     public void setPassword(String password) {
         this.password = password;
     }
+    */
 }
