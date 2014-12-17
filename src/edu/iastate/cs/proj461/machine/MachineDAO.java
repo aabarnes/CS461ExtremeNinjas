b4ecc3690f5066204efe95a3a425bddea0c9a4fa
Patrick <patricke@iastate.edu>
2014-12-17 00:11:55 -0600

    DAO interface and class for Machine.  Working for fetching machine statuses.

--- src/edu/iastate/cs/proj461/machine/MachineDAO.java
+++ src/edu/iastate/cs/proj461/machine/MachineDAO.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 public interface MachineDAO {
 	
-	public List<Machine> getAllMachinesInfo();
-	public Machine getMachineInfo(int id);
-	public Machine getMachineInfo(String ipAddress);
 	public List<Machine> getAllMachines();
+	public Machine getMachine(int id);
+	public Machine getMachine(String ipAddress);
 	public List<String> getMachineIps();
 	public void addMachine(Machine machine);
-
 }
